0666a34feab6ed143da796f4556e3bc51c74ca84
晨魔 <139463472+MorningDemon515@users.noreply.github.com>
2024-04-04 17:55:07 +0800

    2.1

--- Sledge.DataStructures/Properties/AssemblyInfo.cs
+++ Sledge.DataStructures/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Sledge.DataStructures")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("2.0.7.2")]
-[assembly: AssemblyVersion("2.0.7.2")]
-[assembly: AssemblyFileVersion("2.0.7.2")]
+[assembly: AssemblyVersion("2.1")]
+[assembly: AssemblyFileVersion("2.1")]
